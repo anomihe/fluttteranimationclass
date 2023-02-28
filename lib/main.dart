cc9a72e43a97906706efcfffc18bd19396644af5
anomihe <anomihekenneth@gmail.com>
2023-02-28 21:18:25 +1300

    class 3 of animation course

---
+++ lib/main.dart
@@ -0,0 +1,173 @@
+import 'dart:math' show pi;
+
+import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math_64.dart' show Vector3;
+
+void main() {
+  runApp(const MyApp());
+}
+
+const widthAndHeight = 100.0;
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> with TickerProviderStateMixin {
+  late AnimationController _xController;
+  late AnimationController _yController;
+  late AnimationController _zController;
+
+  late Tween<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+    _xController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 20),
+    );
+    _yController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 30),
+    );
+    _zController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 40),
+    );
+    _animation = Tween<double>(
+      begin: 0,
+      end: pi * 2,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _xController.dispose();
+    _yController.dispose();
+    _zController.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _xController
+      ..reset()
+      ..repeat();
+
+    _yController
+      ..reset()
+      ..repeat();
+
+    _zController
+      ..reset()
+      ..repeat();
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text('Animation 3'),
+            ),
+            body: SafeArea(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  const SizedBox(
+                    height: 100,
+                    width: double.infinity,
+                  ),
+                  AnimatedBuilder(
+                      animation: Listenable.merge([
+                        _xController,
+                        _yController,
+                        _zController,
+                      ]),
+                      builder: (context, snapshot) {
+                        return Transform(
+                          alignment: Alignment.center,
+                          transform: Matrix4.identity()
+                            ..rotateX(_animation.evaluate(_xController))
+                            ..rotateY(_animation.evaluate(_yController))
+                            ..rotateZ(_animation.evaluate(_zController)),
+                          child: Stack(
+                            children: [
+                              //back
+                              Transform(
+                                alignment: Alignment.center,
+                                transform: Matrix4.identity()
+                                  ..translate(
+                                    Vector3(
+                                      0,
+                                      0,
+                                      -widthAndHeight,
+                                    ),
+                                  ),
+                                child: Container(
+                                  color: Colors.purple,
+                                  width: widthAndHeight,
+                                  height: widthAndHeight,
+                                ),
+                              ),
+                              //left side
+                              Transform(
+                                alignment: Alignment.centerLeft,
+                                transform: Matrix4.identity()
+                                  ..rotateY(pi / 2.0),
+                                child: Container(
+                                  color: Colors.red,
+                                  width: widthAndHeight,
+                                  height: widthAndHeight,
+                                ),
+                              ),
+                              //right side
+                              Transform(
+                                alignment: Alignment.centerRight,
+                                transform: Matrix4.identity()
+                                  ..rotateY(-pi / 2.0),
+                                child: Container(
+                                  color: Colors.blue,
+                                  width: widthAndHeight,
+                                  height: widthAndHeight,
+                                ),
+                              ),
+                              //fornt
+                              Container(
+                                color: Colors.green,
+                                width: widthAndHeight,
+                                height: widthAndHeight,
+                              ),
+                              //top side
+                              Transform(
+                                alignment: Alignment.topCenter,
+                                transform: Matrix4.identity()..rotateX(-pi / 2),
+                                child: Container(
+                                  color: Colors.orange,
+                                  width: widthAndHeight,
+                                  height: widthAndHeight,
+                                ),
+                              ),
+                              //down
+                              Transform(
+                                alignment: Alignment.bottomCenter,
+                                transform: Matrix4.identity()..rotateX(pi / 2),
+                                child: Container(
+                                  color: Colors.brown,
+                                  width: widthAndHeight,
+                                  height: widthAndHeight,
+                                ),
+                              ),
+                            ],
+                          ),
+                        );
+                      })
+                ],
+              ),
+            )));
+  }
+}
